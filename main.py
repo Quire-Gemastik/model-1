f176e3a8c41479230a18f3566c566c34917b69aa
muhammadraiz <mu115raiz@gmail.com>
2024-07-23 13:17:37 +0700

    Update model

--- main.py
+++ main.py
@@ -135,6 +135,3 @@ async def predict(input: UserInput):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
